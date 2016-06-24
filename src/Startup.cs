3ab7c9f18b6621f77200e5d9eaa4a03297d72029
Austin Felipe <austinfelipe@Austins-iMac.local>
2016-06-24 03:40:11 -0300

    Add Articles rep into DI scope

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,3 +1,4 @@
+using AustinSite.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -25,6 +26,8 @@ namespace AustinSite
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddScoped<ArticlesRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
