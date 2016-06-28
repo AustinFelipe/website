0a1501bd271d9c1c4e1f6cb4251030d1c7ea2776
Austin Felipe <austinfelipe@Austins-iMac.local>
2016-06-27 22:56:53 -0300

    Included a contact action

--- src/Controllers/ContactController.cs
+++ src/Controllers/ContactController.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using AustinSite.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AustinSite.Controllers
@@ -7,6 +9,18 @@ namespace AustinSite.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage([FromServices] ContactEmailService contactEmailService,
+            string name, 
+            string fromEmail, 
+            string message)
+        {
+            await contactEmailService.SendContactFromSite(name, fromEmail, message, "austin.felipe@live.com");
+            await contactEmailService.SendSubscriberEmailAsync(name, fromEmail, message);
+
+            return Ok(new { message = "Thanks for stopping by!" });
         }
     }
 }
