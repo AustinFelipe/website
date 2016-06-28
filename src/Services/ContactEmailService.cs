419fdff04cac4953150a77f9924986c8ef1e7f1e
Austin Felipe <austinfelipe@Austins-iMac.local>
2016-06-27 22:57:13 -0300

    Service that handles email services at the site

---
+++ src/Services/ContactEmailService.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AustinSite.Utils;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AustinSite.Services
+{
+    public class ContactEmailService
+    {
+        private IEmailSender emailService;
+        private IMemoryCache memoryCache;
+
+        public ContactEmailService(IEmailSender emailService,
+            IMemoryCache memoryCache)
+        {
+            this.emailService = emailService;
+            this.memoryCache = memoryCache;
+        }
+
+        public async Task SendContactFromSite(string name, 
+            string fromEmail, 
+            string message,
+            string ownerEmail)
+        {
+            Dictionary<string, string> templateData = new Dictionary<string, string>
+            {
+                { "name", name.GetFirstName() },
+                { "email", fromEmail },
+                { "message", message }
+            };
+
+            var template = RazorUtils.RenderViewToString(memoryCache, 
+                "ContactViaSite", 
+                templateData,
+                $"{name} contact you via Austin's site. Their email {fromEmail}. Their message {message}");
+
+            await emailService.SendEmailAsync(ownerEmail, 
+                "Contact via Austin's Site", 
+                template);
+        }
+
+        public async Task SendSubscriberEmailAsync(string name, 
+            string fromEmail, 
+            string message)
+        {
+            Dictionary<string, string> templateData = new Dictionary<string, string>
+            {
+                { "name", name.GetFirstName() }
+            };
+
+            var template = RazorUtils.RenderViewToString(memoryCache, 
+                "ThanksMessage", 
+                templateData,
+                $"Hi {name.GetFirstName()}, I've received your message. As soon as possible, I will answer you.");
+
+            await emailService.SendEmailAsync(fromEmail, 
+                "I've received your message", 
+                template);
+        }
+    }
+}
