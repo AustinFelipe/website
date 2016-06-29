88207578419bfc0da7ff32a88dcacca432b18a50
Austin Felipe <austinfelipe@Austins-iMac.local>
2016-06-28 23:44:59 -0300

    Missing properties included

--- src/Models/ProjectModel.cs
+++ src/Models/ProjectModel.cs
@@ -10,9 +10,23 @@ namespace AustinSite.Models
 
         public string Title { get; set; }
 
+        public string Company { get; set; }
+
+        public string Link { get; set; }
+
+        public string About { get; set; }
+
+        public string Length { get; set; }
+
         public DateTime BuiltAt { get; set; }
 
+        public List<string> TechsAndTools { get; set; }
+
         public List<string> Tags { get; set; }
+
+        public string FlickrFirstPhotoUrl { get; set; }
+
+        public string FlickrAlbumUrl { get; set; }
 
         public string FormattedBuiltAt()
         {
